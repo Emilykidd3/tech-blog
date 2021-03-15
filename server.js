e0d7cee28e2f5c4a576cc7f534cebeeebb583471
emilykidd3 <e.kidd61@yahoo.com>
2021-03-14 22:41:54 -0700

    changed secret to be hardcoded

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.SECRET,
+    secret: 'Super secret secret',
     cookie: {
         maxAge: 15*60*1000
     },
